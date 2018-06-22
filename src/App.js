3ed699d49e2d9a09c42bed877ba93af644b635bc
Mahmoud Gamal <mahmoudgamal1704@gmail.com>
2018-06-22 15:56:33 +0200

    Design static page without any marker

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+  window.initMap = this.initMap;
+  this.loadJS('https://maps.googleapis.com/maps/api/js?key=AIzaSyBZjYXBlQ61P4-XqTZiWi0wEUGhdDEedF4&v=3&callback=initMap');
+}
+  initMap = () => {
+    var map;
+    map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: 31.199752, lng: 29.893712},
+      zoom: 12
+    });
+}
+ loadJS = (src) => {
+    var ref = window.document.getElementsByTagName("script")[0];
+    var script = window.document.createElement("script");
+    script.src = src;
+    script.async = true;
+    ref.parentNode.insertBefore(script, ref);
+}
+  hidebar = () => {
+  var sidebar = document.getElementById('sidebar');
+  sidebar.classList.toggle("hidden");
+}
+  close = () => {
+   var sidebar = document.getElementById('sidebar');
+  sidebar.classList.toggle("hidden");
+}
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <nav className="w3-sidebar w3-bar-block w3-border-right hidden side" id="sidebar">
+          <a onClick={this.close} className="w3-bar-item w3-large">Close &times;</a>
+          <h3>Gemy Locations</h3>
+          <input type="text" name="search" placeholder="search here"/><span className="fa fa-filter">Filter</span>
+        </nav>
+        <div className="main">
+          <header>
+            <a className="fa fa-bars" id="bar" onClick={this.hidebar}></a>
+          </header>
+          <div id="map"></div>
+        </div>
       </div>
     );
   }
