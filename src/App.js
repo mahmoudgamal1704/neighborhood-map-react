e30fbb99cd41bfa1d81340b942d3af98a69a20a5
Mahmoud Gamal <mahmoudgamal1704@gmail.com>
2018-06-23 15:49:32 +0200

    Edit Search Engine and info window

--- src/App.js
+++ src/App.js
@@ -2,45 +2,56 @@ import React, { Component } from 'react';
 
 import './App.css';
 var map ;
-
+var markers = [] ;
 class App extends Component {
+
 state = {
   locations: [
       {
         name:'Alexandria Faculty of Medicine',
         lat:31.202147,
         lng:29.904264,
-        add:'Al Khartom Sq., Al Mesallah Sharq, Qesm Al Attarin, Alexandria Governorate'
+        add:'Al Khartom Sq., Al Mesallah Sharq, Qesm Al Attarin, Alexandria Governorate',
+        marker:''
       },
       {
         name:'Bibliotheca Alexandrina Conference Center (BACC)',
         lat:31.208243,
         lng:29.908580,
-        add:'El-Gaish Rd, Al Azaritah WA Ash Shatebi, Qesm Bab Sharqi, Alexandria Governorate'
+        add:'El-Gaish Rd, Al Azaritah WA Ash Shatebi, Qesm Bab Sharqi, Alexandria Governorate',
+        marker:''
       },
       {
         name:'Citadel of Qaitbay',
         lat:31.213986,
         lng:29.885598,
-        add:'As Sayalah Sharq, Qesm Al Gomrok, Alexandria Governorate'
+        add:'As Sayalah Sharq, Qesm Al Gomrok, Alexandria Governorate',
+        marker:''
       },
       {
         name:'El Gondy El Maghool Square',
         lat:31.199999,
         lng:29.893725,
-        add:'Al Gonday Al Maghool Sq., Al Mansheyah Al Kubra, Qesm Al Mansheyah, Alexandria Governorate'
+        add:'Al Gonday Al Maghool Sq., Al Mansheyah Al Kubra, Qesm Al Mansheyah, Alexandria Governorate',
+        marker:''
       },
       {
         name:'Raml Station',
         lat:31.200189,
         lng:29.899474,
-        add:'Abou Al Kassm Al Shabi, Al Mesallah Sharq, Qesm Al Attarin, Alexandria Governorate 21532'
+        add:'Abou Al Kassm Al Shabi, Al Mesallah Sharq, Qesm Al Attarin, Alexandria Governorate 21532',
+        marker:''
       }
-  ]
+  ],
+  filterdlocatios: []
 }
   componentDidMount() {
   window.initMap = this.initMap;
   this.loadJS('https://maps.googleapis.com/maps/api/js?key=AIzaSyBZjYXBlQ61P4-XqTZiWi0wEUGhdDEedF4&v=3&callback=initMap');
+  this.setState({
+            filterdlocatios: this.state.locations
+        });
+  
 }
   initMap = () => {
     var map;
@@ -49,19 +60,19 @@ state = {
       zoom: 14
     });
     var marker;
-    var artt ;
     var markers = [] ;
     var largeInfowindow = new window.google.maps.InfoWindow();
-    this.state.locations.map(location => (
-      marker = new window.google.maps.Marker({
+    this.state.filterdlocatios.map(location => (
+      location.marker = new window.google.maps.Marker({
           position: {lat: location.lat , lng: location.lng},
           map: map,
           title: location.name,
           add: location.add
         }),
-      marker.addListener('click', function() {
+      location.marker.addListener('click', function() {
             populateInfoWindow(this, largeInfowindow);
-        })
+        }),
+        markers.push(location.marker)
     ))
    
 }
@@ -81,29 +92,75 @@ state = {
   sidebar.classList.toggle("hidden");
 }
   filterlocations = (event) => {
+  var mak = this.getmarker('Alexandria Faculty of Medicine')
+  console.log(mak)
+  mak.setMap(null);
+  var query = event.target.value;
+  if (query){
   var newlocations = [] ;
-    this.state.locations.map(location => {
-      if ( location.name.toLowerCase().indexOf(event.target.value.toLowerCase()) >= 0 ) {
-
-      newlocations.push(location);
-    }else {
-
-  }
+    this.state.filterdlocatios.map(location => {
+      if ( location.name.toUpperCase().indexOf(query.toUpperCase()) >= 0 ) {
+        newlocations.push(location);
+      }
     })
+    console.log(newlocations.length)
+    console.log(this.state.filterdlocatios.length)
     this.setState({
-            locations: newlocations
+            filterdlocatios: newlocations
         });
+        console.log(this.state.filterdlocatios.length)
+}else {
+  this.setState({
+            filterdlocatios: this.state.locations
+        });
+}
+        this.initMap();
+}
+ delay = (ms) => {
+   ms += new Date().getTime();
+   while (new Date() < ms){}
+}
+ drobdown = () => {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+filter = (event) => {
+  var bar = document.getElementById("txt")
+  bar.value= event.target.innerText;
+  document.getElementById("myDropdown").classList.toggle("show");
+  bar.click();
+}
+refresh = () => {
+ this.setState({
+            filterdlocatios: this.state.locations
+        });
+        document.getElementById("myDropdown").classList.toggle("show");
+        var bar = document.getElementById("txt")
+        bar.value= "";
+        this.initMap();
+}
+open = (event) => {
+  window.google.maps.event.trigger(this.getmarker(event.target.innerText), 'click');
+}
+getmarker = (title) => {
+  var location = this.state.locations.filter(locat => locat.name === title)
+  return location[0].marker
 }
   render() {
     return (
       <div className="App">
-        <nav className="w3-sidebar w3-bar-block w3-border-right hidden side" id="sidebar">
+        <nav className="w3-sidebar w3-bar-block w3-border-right side" id="sidebar">
           <a onClick={this.close} className="w3-bar-item w3-large">Close &times;</a>
           <h3>Gemy Locations</h3>
-          <input type="text" name="search" placeholder="search here" onChange={this.filterlocations}/><span className="fa fa-filter">Filter</span>
+          <input type="text"  name="search" placeholder="search here" onChange={this.filterlocations} onKeyPress={this.filterlocations} onClick={this.filterlocations} id="txt"/><span ><button onClick={this.drobdown} className="dropbtn"><span className="fa fa-filter">Filter</span></button>
+                      <div id="myDropdown" className="dropdown-content">
+                        {this.state.locations.map(location => (
+                            <a key={location.lat} onClick={this.filter}>{location.name}</a>
+                          ))}
+                          <a onClick={this.refresh} > ALL</a>
+                      </div></span>
           <ol>
-            {this.state.locations.map(location => (
-            <li key={location.lat}>{location.name}</li>
+            {this.state.filterdlocatios.map(location => (
+            <li key={location.lat} onClick={this.open}>{location.name}</li>
           ))}
           </ol>
         </nav>
@@ -119,6 +176,8 @@ state = {
 }
 
 export default App;
+
+
 function populateInfoWindow(marker, infowindow) {
     // Check to make sure the infowindow is not already opened on this marker.
     if (infowindow.marker !== marker) {
